e5c03713c186ae9db82975c9d932ba17b05843b2
Amy Fang <amy.t.fang@gmail.com>
2016-12-20 17:25:12 -0500

    Update config files to address minification error

---
+++ webpack.config.prod.js
@@ -0,0 +1,38 @@
+var webpack = require("webpack");
+
+module.exports = {
+  context: __dirname,
+  entry: './frontend/casenote.jsx',
+  output: {
+    path: "./app/assets/javascripts",
+    filename: 'bundle.js'
+  },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ],
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map'
+};
